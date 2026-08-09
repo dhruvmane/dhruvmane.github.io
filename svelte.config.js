5557068a622fbba522a5320f84f39ec6cfeac98f
barden <theofficialbarden@gmail.com>
2026-08-10 01:34:00 +0530

    Add Wrangler

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		runes: true,
-	},
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-			base: ''
-		}
-	},
+	compilerOptions: { runes: true },
+	kit: { adapter: adapter(), paths: { base: '' } },
 	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) =>
-			filename.includes('node_modules') ? undefined : { runes: true }
+		dynamicCompileOptions: ({ filename }) => filename.includes('node_modules') ? undefined : { runes: true }
 	}
 };
 
